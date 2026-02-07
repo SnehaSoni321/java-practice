486a4c875f914b3644354d2472b09e5a3919d900
Sneha <snehasoni598@gmail.com>
2026-02-07 14:10:13 +0530

    Practice for break and continue

---
+++ breakAndContinue.java
@@ -0,0 +1,24 @@
+public class breakAndContinue {
+    public static void main(String args[]) {
+        int arr[] = {1,2,3,4,5};
+
+    // break -> loop se bahar nikal jao
+    // for(int i = 1; i<= 5; i++) {
+    //     if(i == 3) {
+    //         break;
+    //     }
+    //     System.out.println(i);
+    // }
+
+    // continue -> sirf current step skip
+    for(int i = 1; i<=arr.length; i++) {
+        if(i == 3) {
+            continue;
+        }
+        System.out.println(i);
+    }
+
+    }
+    
+    
+}
