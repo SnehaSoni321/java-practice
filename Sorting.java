aebf50849331903d2beeca3fd79f1648c0405cd7
Sneha <snehasoni598@gmail.com>
2026-02-09 16:34:39 +0530

    Sorting practice

--- Sorting.java
+++ Sorting.java
@@ -24,21 +24,40 @@ public class Sorting {
         // printArray(arr);
 
         //selection sort
+        // time complexity = O(n^2)
 
-        int arr[] = {6,4,7,3,1};
+        // int arr[] = {6,4,7,3,1};
 
-        for(int i = 0; i<arr.length-1; i++) {
-            int smallest = i;
-            for(int j=i+1; j<arr.length; j++) {
-                if(arr[smallest] > arr[j]) {
-                    smallest = j;
-                }
+        // for(int i = 0; i<arr.length-1; i++) {
+        //     int smallest = i;
+        //     for(int j=i+1; j<arr.length; j++) {
+        //         if(arr[smallest] > arr[j]) {
+        //             smallest = j;
+        //         }
+        //     }
+        //     int temp = arr[smallest];
+        //     arr[smallest] = arr[i];
+        //     arr[i] = temp;
+        // }
+        // printArray(arr);
+
+        //insertion sort
+
+        int arr[] = {4,5,3,6,2};
+
+        for(int i = 1; i<arr.length; i++) {
+            int current = arr[i];
+            int j = i-1;
+
+            while(j>=0 && current<arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
             }
-            int temp = arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i] = temp;
+            //placement
+            arr[j+1] = current;
         }
         printArray(arr);
+
     }
     
 }
